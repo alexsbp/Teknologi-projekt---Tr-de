2ea3c723d94c212209bb20b864cd2b5389e36886
Windows <lykke.daniel@hotmail.dk>
2017-03-20 11:25:50 +0100

    Worker

---
+++ Træpisseren/Træpisseren/ComponentsPattern/Worker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Træpisseren
+{
+    class Worker : Component, IUpdateable
+    {
+        public Worker(GameObject gameObject) : base(gameObject)
+        {
+            gameObject.Tag = "Worker";
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}

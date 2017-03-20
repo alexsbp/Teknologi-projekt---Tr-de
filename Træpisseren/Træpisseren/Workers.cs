436cb60f2ecf5de635a07efb7d75b86417b318d7
Alex <Alex@ALEXPEDERSEN>
2017-03-20 09:25:36 +0100

    .

--- Træpisseren/Træpisseren/Workers.cs
+++ Træpisseren/Træpisseren/Workers.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Træpisseren
 {
     class Workers
     {
+
     }
 }
